102e9914ca0e91102883df42581f83a3522aedb8
absak <abir.sakif@gmail.com>
2015-09-19 08:31:32 +0600

    First commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Shopping Saga</title>
+<link rel="stylesheet" href="styles/style.css"/>
+</head>
+
+<body>
+	<!--Main Container starts from here-->
+	<div class="main_wrapper">
+    	
+        <!--Header section Starts here-->
+    	<div class="header_wrapper">
+        	<img src="images/logo.JPG" style="float:left"/>
+            <img src="images/images.jpg" style="float:left"/>
+            <img src="images/paypal.jpg" style="float:right"/>
+            
+        </div>
+        <!--Header section Ends here-->
+		
+        <!--Navigation bar Starts here-->
+        <div id="navbar">Navigation Bar</div>
+        <!--Navigation bar Starts here-->
+        
+        <div class="content_wrapper">
+        	<div id="left_sidebar">left content</div>
+            <div id="right_content">right content</div>
+        
+        </div>
+        <div>Footer Area</div>
+    
+    
+    </div>
+    <!--Main Container starts from here-->
+</body>
+</html>
